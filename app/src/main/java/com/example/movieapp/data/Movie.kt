5bbe96d8345f8649637d3e9d7c1fe6db21d2a063
alexsit19 <alexsit12@gmail.com>
2021-03-22 21:54:26 +0300

    add some files

---
+++ app/src/main/java/com/example/movieapp/data/Movie.kt
@@ -0,0 +1,15 @@
+package com.example.movieapp.data
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val poster: String,
+    val backdrop: String,
+    val ratings: Float,
+    val numberOfRatings: Int,
+    val minimumAge: Int,
+    val runtime: Int,
+    val genres: String,
+    val actors: List<Actor>
+)
